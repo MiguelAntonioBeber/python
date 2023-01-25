9976102766d069f87c8ba83762d8a8503d59ce37
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-25 15:45:34 -0300

    Create ex007.py

---
+++ python-exercicios/ex007.py
@@ -0,0 +1,7 @@
+n = float(input('Digite a primeira nota: '))
+n1 = float(input('Digite a segunda nota: '))
+m = (n + n1) / 2
+
+print()
+
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n, n1, m))
