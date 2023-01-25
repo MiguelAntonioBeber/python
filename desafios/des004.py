8162638d05e8bd6d5a4549572d9932c0d4e98ef1
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-25 03:05:22 -0300

    Create des004.py

---
+++ desafios/des004.py
@@ -0,0 +1,18 @@
+n = input('Digite algo: ')
+
+print('O tipo primitivo deste valor é: ',type(n))
+
+print()
+
+print('Está em maiúsculas?', n.isupper())
+print('É um número?', n.isalnum())
+print('É alfabético?', n.isalpha())
+print('É numérico?', n.isnumeric())
+print('É um ASCII?', n.isascii())
+print('É um decimal?', n.isdecimal())
+print('É um digito?', n.isdigit())
+print('É um identificador?', n.isidentifier())
+print('Está em minúsculas?', n.islower())
+print('É possivel imprimir?', n.isprintable())
+print('É um espaço?', n.isspace())
+print('É um título?', n.istitle())
