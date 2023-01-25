4f1353e6a9cf2e9b37625d6bbc16b45e0addea6b
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-25 03:05:17 -0300

    Create aula06.py

---
+++ aulas/aula06.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+# print('A soma entre', n1, 'e', n2, 'vale:', s) Forma 1
+print('A soma entre {0} e {1} vale: {2}'.format(n1, n2, s))
+
+
