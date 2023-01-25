ef0192489d3ed17dbd412fb74b9033d6067213a0
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-25 15:45:32 -0300

    Create ex006.py

---
+++ python-exercicios/ex006.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n**(1/2)
+
+print()
+
+print('O dobro de {} vale {}. \nO triplo de {} vale {}. \nA raiz quadrada de {} vale {}.'.format(n, d, n, t, n, r))
