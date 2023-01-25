713c001f7a4601965a522e898bacb237b80a9794
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-25 15:45:30 -0300

    Create ex005.py

---
+++ python-exercicios/ex005.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))  # input
+a1 = n1 - 1  # antecessor
+s1 = n1 + 1  # sucessor
+
+print('Analisando o valor {0}, seu antecessor é {1} e seu sucessor é {2}'.format(n1, a1, s1))  # resultado
