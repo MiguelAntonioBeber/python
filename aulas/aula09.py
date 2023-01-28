6c3d7193379089cf9d9ecea1097196358a6a6e9f
Miguel Antonio Beber <112721503+MiguelAntonioBeber@users.noreply.github.com>
2023-01-28 18:52:05 -0300

    Create aula09.py

---
+++ aulas/aula09.py
@@ -0,0 +1,20 @@
+frase = 'Curso em Vídeo Python'
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('Curso' in frase)
+print(frase.lower().find('vídeo'))
+dividido = frase.split()
+print(dividido[2][3])
+
+print("""Welcome! Are you completely new to programming?
+If not then we presume you will be looking for information
+about why and how to get started with Python. Fortunately
+an experienced programmer in any programming language
+(whatever it may be) can pick up Python very quickly.
+Its also easy for begginers to use and learn, so jump in!""")
+
+## ln 4: verifica se tem 'Curso' na frase
+## ln 5: Procura 'vídeo' na frase em minúscula
+## ln 6: divide a frase
+##ln 7: Mostra o terceiro caractere da segunda palavra do texto dividido
+## ln 9:15 = esscreve um texto inteiro
